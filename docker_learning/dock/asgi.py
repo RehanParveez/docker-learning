492e0dedbee5c3d34c18df614dda9e680e36d82a
Rehan Parveez <rehanparveez@gmail.com>
2026-05-31 14:13:06 +0500

    this c/m is a.b work on docke.pgadmin&postgres, inv.vol&cus net

--- docker_learning/dock/asgi.py
+++ docker_learning/dock/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for proj project.
+ASGI config for dock project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dock.settings')
 
 application = get_asgi_application()
